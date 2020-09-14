f260c56733fcaef90d07c5fc27522fafb1098930
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-14 07:59:02 +0530

    Added to load India census data

--- main/CensusAnalyser.js
+++ main/CensusAnalyser.js
@@ -1,2 +1,17 @@
-var msg = "Welcome to census analyser program"
+var msg = "Welcome to census analyser program";
 console.log(msg);
+
+module.exports = function (csvFile, callback) {
+  const csv = require("csv-parser");
+  const fs = require("fs");
+  let count = 0;
+  fs.createReadStream("IndiaStateCensusData.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+      count += 1;
+    })
+    .on("end", () => {
+      console.log("Total count: " + count);
+      return callback(count);
+    });
+};
