21309a91bd0fb402607c4a454be6cff5630531c9
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-14 08:00:47 +0530

    Added test to check India state census file

---
+++ test/CensusAnalyserTest.js
@@ -0,0 +1,11 @@
+const assert = require("chai").assert;
+const CensusAnalyserController = require("../main/CensusAnalyser");
+const INDIA_STATE_CENSUS_FILE = "./IndiaStateCensusData.csv";
+
+describe("loadIndianStateCensusData", function () {
+  it("it loads Indian census data", function () {
+    CensusAnalyserController(INDIA_STATE_CENSUS_FILE, function (data) {
+      assert.equal(data, 29);
+    });
+  });
+});
