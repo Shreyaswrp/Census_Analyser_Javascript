538c264bef384c2377bef841bc0bcf80c6f2223b
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-15 17:17:19 +0530

    Refactored to achieve DRY

--- main/CensusAnalyser.js
+++ main/CensusAnalyser.js
@@ -4,6 +4,14 @@ const csvToJson = require("csvtojson");
 
 console.log ("Welcome to census analyser program");
 
+function csvToJsonConversion(csvFile,callback){
+  csvToJson()
+  .fromFile(csvFile)
+  .then((data) => {
+  return callback(data);
+  });  
+}
+
 function loadCSVFileData(csvFile, callback) {
     let count = 0;
     fs.createReadStream(csvFile)
@@ -12,51 +20,40 @@ function loadCSVFileData(csvFile, callback) {
       count += 1;
     })
     .on("end", () => {
-      console.log("Total count: " + count);
       return callback(count);
     });
 }
 
 function getSortedDataByState(csvFile, callback) {
-    csvToJson()
-    .fromFile(csvFile)
-    .then((data) => {
+    csvToJsonConversion(csvFile,function (data){
     data.sort((a, b) => a.State.localeCompare(b.State));
     return callback(data);
     });
 }
 
 function getSortedDataByStateCode(csvFile, callback) {
-    csvToJson()
-    .fromFile(csvFile)
-    .then((data) => {
+    csvToJsonConversion(csvFile,function (data){
     data.sort((a, b) => a.StateCode.localeCompare(b.StateCode));
     return callback(data);
     });
 }
 
 function getSortedDataByPopulation(csvFile, callback) {
-    csvToJson()
-    .fromFile(csvFile)
-    .then((data) => {
+    csvToJsonConversion(csvFile,function (data){
     data.sort((a, b) => a.Population - b.Population);
     return callback(data);
     });
 }
 
 function getSortedDataByDensity(csvFile, callback) {
-    csvToJson()
-    .fromFile(csvFile)
-    .then((data) => {
+    csvToJsonConversion(csvFile,function (data){
     data.sort((a, b) => a.DensityPerSqKm - b.DensityPerSqKm);
     return callback(data);
     });
 }
 
 function getSortedDataByArea(csvFile, callback) {
-    csvToJson()
-    .fromFile(csvFile)
-    .then((data) => {
+    csvToJsonConversion(csvFile,function (data){
     data.sort((a, b) => a.AreaInSqKm - b.AreaInSqKm);
     return callback(data);
     });
