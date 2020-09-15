e3bd60f01c7ab3a73d72817ea3f6140e39bf88fe
Shreya Swaroop <shreyaswrp@gmail.com>
2020-09-15 16:56:45 +0530

    Refactored to remove unused variables

--- main/CensusAnalyser.js
+++ main/CensusAnalyser.js
@@ -1,37 +1,37 @@
-console.log ("Welcome to census analyser program");
-
 const csv = require("csv-parser");
 const fs = require("fs");
 const csvToJson = require("csvtojson");
+
+console.log ("Welcome to census analyser program");
 
 function loadCSVFileData(csvFile, callback) {
   let count = 0;
   fs.createReadStream(csvFile)
     .pipe(csv())
-    .on("data", (row) => {
+    .on("data" , () => {
       count += 1;
     })
     .on("end", () => {
       console.log("Total count: " + count);
       return callback(count);
-      });
+    });
 }
 
 function getSortedDataByState(csvFile, callback) {
-  csvToJson()
+    csvToJson()
     .fromFile(csvFile)
     .then((data) => {
-      data.sort((a, b) => a.State.localeCompare(b.State));
-      return callback(data);
+    data.sort((a, b) => a.State.localeCompare(b.State));
+    return callback(data);
     });
 }
 
 function getSortedDataByStateCode(csvFile, callback) {
-  csvToJson()
+    csvToJson()
     .fromFile(csvFile)
     .then((data) => {
-      data.sort((a, b) => a.StateCode.localeCompare(b.StateCode));
-      return callback(data);
+    data.sort((a, b) => a.StateCode.localeCompare(b.StateCode));
+    return callback(data);
     });
 }
 
@@ -39,26 +39,26 @@ function getSortedDataByPopulation(csvFile, callback) {
     csvToJson()
     .fromFile(csvFile)
     .then((data) => {
-      data.sort((a, b) => a.Population - b.Population);
-      return callback(data);
+    data.sort((a, b) => a.Population - b.Population);
+    return callback(data);
     });
 }
 
 function getSortedDataByDensity(csvFile, callback) {
-  csvToJson()
+    csvToJson()
     .fromFile(csvFile)
     .then((data) => {
-      data.sort((a, b) => a.DensityPerSqKm - b.DensityPerSqKm);
-      return callback(data);
+    data.sort((a, b) => a.DensityPerSqKm - b.DensityPerSqKm);
+    return callback(data);
     });
 }
 
 function getSortedDataByArea(csvFile, callback) {
-  csvToJson()
+    csvToJson()
     .fromFile(csvFile)
     .then((data) => {
-      data.sort((a, b) => a.AreaInSqKm - b.AreaInSqKm);
-      return callback(data);
+    data.sort((a, b) => a.AreaInSqKm - b.AreaInSqKm);
+    return callback(data);
     });
 }
 
